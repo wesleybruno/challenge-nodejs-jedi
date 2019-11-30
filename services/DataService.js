e71b877e2c359850cb2ce0674533899c39aa59f5
Wesley <wesleybruno.so@gmail.com>
2019-11-30 13:55:00 -0300

    fix DataService

--- services/DataService.js
+++ services/DataService.js
@@ -1,22 +1,26 @@
 const axios = require('axios');
-const jsonData = require('./../dataJson.json')
 const fs = require('fs');
 exports.getJsonData = async () => {
 
     try{
 
-        if(jsonData)
+        let jsonData
+
+        try {
+            jsonData = require('./../dataJson.json')
+
             return jsonData
+        } catch (ex) {
+            const response  = await axios.get('https://raw.githubusercontent.com/LiveOnSolutions/challenge-nodejs-jedi/master/chaotic_data.json')
 
-        const response  = await axios.get('https://raw.githubusercontent.com/LiveOnSolutions/challenge-nodejs-jedi/master/chaotic_data.json')
+            fs.writeFile('dataJson.json', JSON.stringify(response.data), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
 
-        fs.writeFile('dataJson.json', JSON.stringify(data), (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
+            return response.data
+        }
 
-         return response.data
-        
     } catch(e){
         console.error('Erro ao buscar dados no servidor', e.toString())
     }

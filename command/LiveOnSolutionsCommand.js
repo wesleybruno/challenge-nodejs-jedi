94467c330626b96e0ce696af8f6a4f7c30469199
Wesley <wesleybruno.so@gmail.com>
2019-11-30 10:43:05 -0300

    change to class

--- command/LiveOnSolutionsCommand.js
+++ command/LiveOnSolutionsCommand.js
@@ -1,56 +1,83 @@
 const dataService = require('./../services/DataService')
 const _ = require('lodash')
 
-exports.orderByStatus = async () => {
-    try{
-        const service = require('./../services/DataService')
-        const arrayData = await service.getJsonData()
-        
-        const arrayOrdened = _.orderBy(arrayData, ['status'],['asc'])
-        return arrayOrdened
-    } catch(e){
-        console.error(e.toString())
-        return false
+module.exports = new class Order {
+    
+    async orderByStatus() {
+        try{
+            const service = require('./../services/DataService')
+            const arrayData = await service.getJsonData()
+            
+            const arrayOrdened = _.orderBy(arrayData, ['status'],['asc'])
+            return arrayOrdened
+        } catch(e){
+            console.error(e.toString())
+            return false
+        }
     }
-};
+
+    async orderTotal(status) {
+        try{
+            const service = require('./../services/DataService')
+            const arrayData = await service.getJsonData()
+
+            const arrayFilter = arrayData.filter( (element) => {
+                return element.status == status
+            })
+            
+            let valorTotal = 0
+
+            arrayFilter.forEach(element => {
+
+                const {order_items} = element
+
+                if(order_items){
+                    valorTotal = _.reduce(order_items, function(sum, order) {
+                        return sum + order.amount;
+                    }, 0);
+                }
+
+            });
+
+            return valorTotal
+        }catch(e){
+            console.error(e.toString())
+            return false
+        }
+    };
 
 
-exports.orderTotal = async (status) => {
-    try{
-        const service = require('./../services/DataService')
-        const arrayData = await service.getJsonData()
+    async orderMajorValue() {
+        try{
+            const service = require('./../services/DataService')
+            const arrayData = await service.getJsonData()
 
-        const arrayFilter = arrayData.filter( (element) => {
-            return element.status == status
-        })
-        
-        let valorTotal = 0
+            const arrayStatus = this._getAllStatus(arrayData)
 
-        arrayFilter.forEach(element => {
+            return arrayStatus
+        } catch(e) {
+            console.error(e.toString())
+            return false
+        }
+    };
 
-            const {order_items} = element
+    
+    async groupByCountry() {
+        return new Error('Not Impremented yet')
+    };
 
-            if(order_items){
-                valorTotal = _.reduce(order_items, function(sum, order) {
-                    return sum + order.amount;
-                }, 0);
-            }
+    _getAllStatus(data) {
+        try{
 
-        });
+            const arrayStatus = data.map( element => element.status)
 
-        return valorTotal
-    }catch(e){
-        console.error(e.toString())
-        return false
+            const arrayUnique = [...new Set(arrayStatus)]
+
+            return arrayUnique
+
+        } catch(e){
+            return []
+        }
     }
-};
 
-
-exports.orderMajorValue = async () => {
-    return new Error('Not Impremented yet')
-};
-
-
-exports.groupByCountry = async () => {
-    return new Error('Not Impremented yet')
-};
+}
